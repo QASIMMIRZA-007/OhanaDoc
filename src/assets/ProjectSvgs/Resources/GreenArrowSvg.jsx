f8806ccb42f0f8eb8bcaee7bf4310a3078018764
QASIM_DEvELOPER <mirzaq620@gamil.com>
2023-12-08 17:47:33 +0500

    error fixed

---
+++ src/assets/ProjectSvgs/Resources/GreenArrowSvg.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function GreenArrowSvg() {
+  return (
+    <div>
+        <svg width="13" height="15" viewBox="0 0 13 15" fill="none" className='arrowgreensvg' xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4523_21576)">
+<path d="M-3.8147e-06 7.50004C-3.8147e-06 7.24223 0.214728 7.03129 0.481692 7.03129H11.4298L7.34702 2.65727C7.16836 2.46593 7.18293 2.16918 7.37947 1.99545C7.47209 1.91606 7.58816 1.87504 7.70423 1.87504C7.83496 1.87504 7.96566 1.92631 8.06057 2.02885L12.8746 7.1851C13.0415 7.36364 13.0415 7.63657 12.8746 7.81499L8.06057 12.9712C7.88095 13.1635 7.57627 13.1763 7.3801 13.0033C7.18357 12.8293 7.169 12.5328 7.34766 12.3415L11.4298 7.96879H0.481401C0.214727 7.96879 -3.8147e-06 7.75786 -3.8147e-06 7.50004Z" fill="#25D366"/>
+</g>
+<defs>
+<clipPath id="clip0_4523_21576">
+<rect width="13" height="15" fill="white" transform="matrix(-1 0 0 -1 13 15)"/>
+</clipPath>
+</defs>
+</svg>
+    </div>
+  )
+}
+
+export default GreenArrowSvg
